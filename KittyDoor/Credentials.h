0ff54bd9ed6c8e523a881b15170a46e0a72089c9
Matt Steinhardt <Matthew.steinhardt@gmail.com>
2021-12-27 19:27:37 -0800

    Complete Rewrite
    - Rewritten with new firebase library

---
+++ KittyDoor/Credentials.h
@@ -0,0 +1,11 @@
+#pragma once
+static const String WIFI_AP_NAME = "ENTER_AP_NAME_HERE";
+static const String WIFI_AP_PASS = "ENTER_WIFI_PASSWORD_HERE";
+
+// Newer Authentication method... unsure if this works
+#define FIREBASE_API "ENTER_WEB_API_KEY"
+#define FIREBASE_USERNAME "ENTER_ADMIN_EMAIL"
+#define FIREBASE_PASS "ENTER_ADMIN_EMAIL_PASS"
+
+// Legacy token Authentication method - Can be found under Project Settings -> Service Accounts
+#define FIREBASE_SECRET "ENTER_SECRET_HERE"
--- KittyDoor/KittyDoor.h
+++ KittyDoor/KittyDoor.h
@@ -1,69 +1,73 @@
-#include <Arduino.h>
-#include "FirebaseESP8266.h"
-#include <ESP8266WiFi.h>
-// #include <FirebaseJson.h>
-#include "WifiCreds.h"
+#pragma once
+#define FIREBASE_HOST "home-controller-286c0.firebaseio.com"
+#define FIREBASE_AUTH "ldaTh4Iy2foRFvAPfdlib5kPMyYchWKF1RKdUGDQ"
+
+#pragma region Pins
+    const int PIN_LIGHT_SENSOR = A0;
+    const int PIN_UP_SENSE = 4;
+    const int PIN_DOWN_SENSE = 12;
+    const int PIN_FORCE_OPEN = 14;
+    const int PIN_FORCE_CLOSE = 13;
+    const int PIN_OPEN_MOTOR = 5;
+    const int PIN_CLOSE_MOTOR = 2;
+#pragma endregion
+
+#pragma region Firebase Paths
+    static const String PATH_STATUS_DOOR = "status/kitty_door/";
+    static const String PATH_STATUS_HW_OVERRIDE = "status/kitty_door_hw_override";
+    static const String PATH_STATUS_LIGHT_LEVEL = "status/kitty_door_light_level";
+    static const String PATH_OPTIONS = "systems/kitty_door";
+    static const String PATH_RESTART_COUNT = "debug/kitty_door/restart_count";
+    static const String PATH_DEBUG_MESSAGE = "debug/kitty_door/messages/";
+#pragma endregion
+
+#pragma region Arduino Constants
+    static const String STATUS_OPEN = "OPEN";
+    static const String STATUS_CLOSED = "CLOSED";
+    static const String STATUS_OPENING = "OPENING";
+    static const String STATUS_CLOSING = "CLOSING";
+
+    static const String NONE = "_none_";
+
+    static const String COMMAND_OPEN = "openKittyDoor";
+    static const String COMMAND_CLOSE = "closeKittyDoor";
+    static const String COMMAND_READ_LIGHT_LEVEL = "readLightLevel";
+
+    const int MAX_LIGHT_LEVEL = 1023;
+    const int MIN_LIGHT_LEVEL = 0;
+#pragma endregion
 
 
-static const String FIREBASE_HOST = "home-controller-286c0.firebaseio.com";
-static const String FIREBASE_AUTH = "ldaTh4Iy2foRFvAPfdlib5kPMyYchWKF1RKdUGDQ";
+#pragma region Structures
+    typedef struct DoorStatus {
+        String old;
+        String desired;
+        String current;
+    } DoorStatus;
 
-/* -------------------- Arduino Constants -------------------- */
-static const String STATUS_OPEN = "OPEN";
-static const String STATUS_CLOSED = "CLOSED";
-static const String STATUS_OPENING = "OPENING";
-static const String STATUS_CLOSING = "CLOSING";
+    typedef struct DebugValues {
+        unsigned int logCounter;
+    } DebugValues;
 
-static const String NONE = "_none_";
+    typedef struct KittyDoorValues {
+        int lightLevel;
+        int hwOverride;
+        int upSense;
+        int downSense;
+        int forceOpen;
+        int forceClose;
+        long delayClosing;
+        long delayOpening;
+    } KittyDoorValues;
 
-static const String COMMAND_OPEN = "openKittyDoor";
-static const String COMMAND_CLOSE = "closeKittyDoor";
-static const String COMMAND_READ_LIGHT_LEVEL = "readLightLevel";
-
-const int MAX_LIGHT_LEVEL = 1023;
-const int MIN_LIGHT_LEVEL = 0;
-
-/* -------------------- Pin Values -------------------- */
-const int PIN_LIGHT_SENSOR = A0;
-const int PIN_UP_SENSE = 4;
-const int PIN_DOWN_SENSE = 12;
-const int PIN_FORCE_OPEN = 14;
-const int PIN_FORCE_CLOSE = 13;
-
-// ??
-const int PIN_OPEN_MOTOR = 5;
-const int PIN_CLOSE_MOTOR = 2;
-
-/* -------------------- Firebase Paths -------------------- */
-static const String PATH_STATUS_DOOR = "status/kitty_door/";
-static const String PATH_STATUS_HW_OVERRIDE = "status/kitty_door_hw_override";
-static const String PATH_STATUS_LIGHT_LEVEL = "status/kitty_door_light_level";
-static const String PATH_OPTIONS = "systems/kitty_door";
-static const String PATH_RESTART_COUNT = "debug/kitty_door/restart_count";
-static const String PATH_DEBUG_MESSAGE = "debug/kitty_door/messages/";
-
-// CUSTOME FIREBASE DEFINES
-#define FIREBASE_PIN_A0 88
-
-String doorStatus = NONE;
-typedef struct KittyDoorValues {
-    int lightLevel;
-    int hwOverride;
-    int upSense;
-    int downSense;
-    int forceOpen;
-    int forceClose;
-    long delayClosing;
-    long delayOpening;
-} KittyDoorValues;
-
-typedef struct KittyDoorOptions {
-    int openLightLevel;
-    int closeLightLevel;
-    int delayOpeningVal;
-    int delayClosingVal;
-    bool delayOpening;
-    bool delayClosing;
-    bool overrideAuto;
-} KittyDoorOptions;
+    typedef struct KittyDoorOptions {
+        int openLightLevel;
+        int closeLightLevel;
+        int delayOpeningVal;
+        int delayClosingVal;
+        bool delayOpening;
+        bool delayClosing;
+        bool overrideAuto;
+    } KittyDoorOptions;
+#pragma endregion
 
